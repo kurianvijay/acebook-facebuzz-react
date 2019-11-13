074cf013862fa155765e3b1be42fe3b5e1d770dc
AlecDWalker <walkeralec@hotmail.com>
2019-11-13 16:55:56 +0000

    edit and delte buttons added (non-functional)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,9 +4,9 @@ class Post extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-      author: null,
-      body: null,
-      timestamp: null,
+      author: 'Alec Walker',
+      body: 'This is a post',
+      timestamp: '7 Oct 2019 7:14.pm',
     }
   }
 render(){
@@ -14,15 +14,16 @@ render(){
     <div className="row mb-1 mt-3">
         <div className="col-12">
           <div className="post-author px-2">
-            Written by: Alec Walker
+            Written by: {this.state.author}
           </div>
           <div className="post-body px-2 mb-2">
-            Content: This is a post
+            Content: {this.state.body}
           </div>
           <div className="post-timestamp px-2">
-            Written at: 7 Oct 2019 7:14pm
+            Written at: {this.state.timestamp}
           </div>
-
+          <button> Edit </button>
+          <button> Delete </button>
         </div>
       </div>
   )
