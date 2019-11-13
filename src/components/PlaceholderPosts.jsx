08d42d5e79a977d30b72316f8a653a1159326548
JDtheGeek <john.dennehy@gmail.com>
2019-11-13 17:14:43 +0000

    Adds placeholder components and a basic grid layout to App.css.  Feel free to replace the placeholders and styling as you add the real components

---
+++ src/components/PlaceholderPosts.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../App.css';
+
+function Posts() {
+  return (
+    <div class='posts' >
+      <p>In here are the posts</p>
+      <div class='post'>
+        <p>This is a post</p>
+      </div>
+      <br />
+      <div class='post'>
+        <p>This is a post</p>
+      </div>
+      <br />
+      <div class='post'>
+        <p>This is a post</p>
+      </div>
+      <br />
+    </div>
+  );
+}
+
+export default Posts;
