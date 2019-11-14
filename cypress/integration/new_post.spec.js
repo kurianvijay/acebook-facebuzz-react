21b216b6db9e912d4c9916968d58b8ed20f768b4
Melvin Lau <lau.melvin@icloud.com>
2019-11-14 14:47:40 +0000

    added test for displaying post once created

--- cypress/integration/new_post.spec.js
+++ cypress/integration/new_post.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
 
-describe('Post form', function () {
+describe('Post', function () {
 
   const url = 'localhost:3000/'
 
@@ -8,20 +8,27 @@ describe('Post form', function () {
     cy.visit(url)
   })
 
-  it('has a submit button', function () {
+  it('form has a submit button', function () {
     cy.get('.post-form')
       .should('have.text', 'Add Post')
   })
 
-  it('has a text-area', function () {
+  it('form has a text-area', function () {
     cy.get('.post-form')
       .should('include.html', 'textarea')
   })
 
-  it('resets its contents after a post has been submitted', function() {
+  it('form textarea resets contents after post submitted', function() {
     cy.get('.post-form textarea').type('Hello')
     cy.get('.post-form button').click()
     cy.get('.post-form textarea').should('have.value', '')
   })
+
+  it('creates a new post in the feed', function () {
+    cy.get('.post-form textarea').type('Hello test1234abcd')
+    cy.get('.post-form button').click()
+    cy.get('body').should('have.text', 'Hello test1234abcd')
+  })
+
 
 })
